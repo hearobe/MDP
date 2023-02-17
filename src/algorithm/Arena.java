81fd1431ea4b85c0215a40f40a2ef6c3dfc6c98d
hearobe <tan.yl1210@gmail.com>
2023-02-17 11:41:53 +0800

    refactor

--- src/algorithm/Arena.java
+++ src/algorithm/Arena.java
@@ -3,11 +3,14 @@ package algorithm;
 import java.util.ArrayList;
 
 import static algorithm.Constants.*;
+import static algorithm.Direction.*;
+import static algorithm.Direction.LEFT;
 
 public class Arena {
 
-    Cell[][] grid;
-    ArrayList<Waypoint> goals = new ArrayList<>();
+    private Cell[][] grid;
+    private Waypoint[] obstacles;
+
 
     public Arena() {
         grid = new Cell[GRID_SIZE_IN_CELLS][GRID_SIZE_IN_CELLS];
@@ -23,13 +26,18 @@ public class Arena {
         }
     }
 
-    // add obstacle positions
-    // add node waypoints for start position and obstacle viewing points
-    // robot start position
-    public void initArenaObstacles(int[] x, int[] y, Direction[] d) {
-        int length = x.length;
+    public Waypoint[] getObstacles() {
+        return obstacles;
+    }
 
-        for (int i = 0; i<length; i++) {
+    public void setObstacles(int[] x, int[] y, Direction[] d) {
+        int n = x.length;
+        obstacles = new Waypoint[n];
+        for (int i = 0; i<n; i++) {
+            obstacles[i] = new Waypoint(x[i]*10 + 5, y[i]*10 + 5, d[i]);
+        }
+
+        for (int i = 0; i<n; i++) {
             int xCoordinate = x[i];
             int yCoordinate = y[i];
 
@@ -55,6 +63,7 @@ public class Arena {
             grid[i][19].setObstacle(true);
         }
     }
+
 
     // currently unused, may not be necessary
     public boolean validCell(Waypoint p) {
---
+++ src/algorithm/PathSequencer.java
@@ -0,0 +1,108 @@
+package algorithm;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static algorithm.Direction.*;
+
+public class PathSequencer {
+    private PathFinder pathFinder = new PathFinder();
+    private Arena arena;
+    private Path[][] pathMatrix;
+    private int[][] costMatrix;
+    private Waypoint[] goals;
+
+    public PathSequencer(Arena arena, Waypoint start) {
+        this.arena = arena;
+
+        calcGoals(start);
+    }
+
+    public List<List<CarCoordinate>> getPath() {
+        fillPathMatrix();
+        List<List<CarCoordinate>> carCoordinates = new ArrayList<>();
+        Waypoint[] obstacles = arena.getObstacles();
+
+        boolean isPathPossible = true;
+        long minCost = Long.MAX_VALUE;
+        long curCost = 0;
+        int[] minPermutation = new int[obstacles.length];
+
+        Permute p = new Permute(obstacles.length);
+        ArrayList<int[]> permutations = p.getPermutations();
+
+        for(int[] a : permutations) {
+            if (costMatrix[0][a[0]] == 0) {
+                continue;
+            }
+            curCost = costMatrix[0][a[0]];
+            for(int i = 1; i<a.length; i++) {
+                if (costMatrix[i-1][i] == 0) {
+                    isPathPossible = false;
+                    break;
+                }
+                curCost += costMatrix[i-1][i];
+            }
+            if (!isPathPossible) {
+                continue;
+            }
+            if (curCost < minCost) {
+                minCost = curCost;
+                minPermutation = Arrays.copyOf(a, a.length);
+            }
+        }
+        System.out.println(Arrays.toString(minPermutation));
+
+        carCoordinates.add(pathMatrix[0][minPermutation[0]].getCarCoordinates());
+        for (int i = 1; i<minPermutation.length; i++) {
+            carCoordinates.add(pathMatrix[i-1][i].getCarCoordinates());
+        }
+
+        return carCoordinates;
+    }
+
+    private void fillPathMatrix() {
+        int n = goals.length;
+        pathMatrix = new Path[n][n];
+        costMatrix = new int[n][n];
+        for (int i = 0; i<n; i++) {
+            for (int j = 1; j<n; j++) {
+                System.out.println("finding path between " + i + " and " + j);
+                pathMatrix[i][j] = pathFinder.findPathBetweenTwoNodes(goals[i], goals[j], arena);
+                if (pathMatrix[i][j] != null) {
+                    costMatrix[i][j] = pathMatrix[i][j].getCost();
+                    System.out.println("path between " + i + " and " + j + " found.");
+                }
+            }
+        }
+    }
+
+
+    private void calcGoals(Waypoint start) {
+        Waypoint[] obstacles = arena.getObstacles();
+        goals = new Waypoint[obstacles.length+1];
+        goals[0] = start;
+        for (int i = 1; i <= obstacles.length; i++) {
+            switch (obstacles[i-1].getDirection()) {
+                case UP:
+                    goals[i] = new Waypoint(obstacles[i-1].getCoordinateX(), obstacles[i-1].getCoordinateY() + 25, Direction.values()[(UP.ordinal()+2) % 4]);
+                    break;
+                case DOWN:
+                    goals[i] = new Waypoint(obstacles[i-1].getCoordinateX(), obstacles[i-1].getCoordinateY() - 25, Direction.values()[(DOWN.ordinal()+2) % 4]);
+                    break;
+                case RIGHT:
+                    goals[i] = new Waypoint(obstacles[i-1].getCoordinateX() + 25, obstacles[i-1].getCoordinateY(), Direction.values()[(RIGHT.ordinal()+2) % 4]);
+                    break;
+                case LEFT:
+                    goals[i] = new Waypoint(obstacles[i-1].getCoordinateX() - 25, obstacles[i-1].getCoordinateY(), Direction.values()[(LEFT.ordinal()+2) % 4]);
+                    break;
+                default:
+                    System.out.println("Error in calcGoals");
+                    break;
+            }
+        }
+    }
+
+
+}
